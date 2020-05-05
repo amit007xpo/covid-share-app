782ef948d9251708f66c7c5d7bda7cc6469b201b
amit_edbot <amitk@mirafra.com>
2020-05-05 18:28:26 +0530

    hydrate-ssr: changed aspect ratio to 4by3

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -224,13 +224,13 @@ onPlayClick = () => {
         <div className={`${baseClassName}__overlay`}></div>
         <div className={`${baseClassName}__video-div`}>
             {
-                videoLibrary && <ResponsiveEmbed aspectRatio={"1by1"}>
+                videoLibrary && <ResponsiveEmbed aspectRatio={window.screen.width > 648 ? "4by3" : "1by1"}>
                     <iframe src={videoUrl}>
                     </iframe>
                     </ResponsiveEmbed>
             }
             {
-                !videoLibrary &&  <ResponsiveEmbed aspectRatio={window.screen.width > 648 ? "16by9" : "1by1"}>
+                !videoLibrary &&  <ResponsiveEmbed aspectRatio={window.screen.width > 648 ? "4by3" : "1by1"}>
                 <video controls className={`${baseClassName}__video-tag`} controls={false} id="myVideo">
                         <source src={url} type="video/mp4" media="screen and (min-device-width:401px)"/>
                         {/* <source src="movie.ogg" type="video/ogg" /> */}
